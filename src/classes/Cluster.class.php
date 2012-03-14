dbc69e82aba80e8cbfc4f115b61f9bf7e969a7af
Anthony Shaw <anthony.p.shaw@gmail.com>
2012-03-14 08:23:32 +0000

    phpdoc

--- src/classes/Cluster.class.php
+++ src/classes/Cluster.class.php
@@ -1,28 +1,132 @@
 <?php
-
+/** 
+ * PHP Requirements - N/A
+ * @package auto-scaler
+ */
+ 
+/** 
+ * Cluster class, represents a Virtual Appliance on which you add/remove Virtual Machines according to the metrics
+ * @package auto-scaler
+ */
 class Cluster {
+	/** 
+	 * Create a new cluster
+	 * @access public
+	 * @static
+	 * @param string $name Name of the cluster
+	 * @return Object|bool Cluster object on success, false on failure
+	 */
 	public static function CreateCluster ( $name ) {
 		$name = DB::Sanitise($name);
-		DB::Query ( "INSERT INTO `clusters` (`clusterName`) VALUES ('$name')" );
+		$res = DB::Query ( "INSERT INTO `clusters` (`clusterName`) VALUES ('$name')" );
+		if ($res)
+			return new Cluster (mysql_insert_id());
+		else 
+			return false;
 	}
+	
+	/**
+	 * Get a list of clusters on the system for the specified user
+	 * @access public
+	 * @static
+	 * @param int $uid User ID (assumes current user if not specified)
+	 * @return array List of cluster records
+	 */
 	public static function GetClusters($uid=false){
 		if (!$uid) $uid = Auth::GetUID();
 		return DB::GetData("SELECT * FROM `clusters` WHERE `customerId`=$uid;");
 	}
 	
+	/**
+	 * ID of the Cluster
+	 * @var int
+	 * @access private
+	 */
 	private $clusterId;
-	public $clusterName; // Friendly name of the cluster
-    public $customerId; // UID of the user/customer
-	public $minServers; // Minimum number of servers required in cluster
-	public $maxServers; // Maximum number of servers required in cluster
-	public $targetVlanId; // Target VLAN ID (ID in backend, will lookup on name change)
-	public $targetVlanName; // Target VLAN Name
-	public $targetApplianceId; // Target Virtual Appliance ID, 
-	public $targetApplianceName; // Target Virtual Appliance Name
-	public $targetVdcName; // Name of the Virtual Data Center'
-	public $targetVdcId; // ID of the VDC
-	public $dateCreated; // Date the cluster was created 
-	public $dateChanged; // Date the cluster was last changed.
+	
+	/**
+	 * Friendly name of the cluster
+	 * @var string
+	 * @access public
+	 */
+	public $clusterName;
+	
+	/**
+	 * UID of the user/customer
+	 * @var int
+	 * @access public
+	 */
+    public $customerId;
+
+	/**
+	 * Minimum number of servers required in cluster
+	 * @var int
+	 * @access public
+	 */
+	public $minServers; 
+	
+	/**
+	 * Maximum number of servers required in cluster
+	 * @var int
+	 * @access public
+	 */
+	public $maxServers; 
+	
+	/**
+	 * Target VLAN ID (ID in backend, will lookup on name change)
+	 * @var int
+	 * @access public
+	 */
+	public $targetVlanId;
+
+	/**
+	 * Target VLAN Name
+	 * @var string
+	 * @access public
+	 */
+	public $targetVlanName; 
+	
+	/**
+	 * Target Virtual Appliance ID
+	 * @var int
+	 * @access public
+	 */	
+	public $targetApplianceId; 
+	
+	/**
+	 * Target Virtual Appliance Name
+	 * @var string
+	 * @access public
+	 */	
+	public $targetApplianceName;
+	
+	/**
+	 * Name of the Virtual Data Center
+	 * @var string
+	 * @access public
+	 */	
+	public $targetVdcName; 
+	
+	/**
+	 * ID of the VDC
+	 * @var int
+	 * @access public
+	 */	
+	public $targetVdcId; 
+	
+	/**
+	 * Date/Time the cluster was created 
+	 * @var string
+	 * @access public
+	 */	
+	public $dateCreated; 
+	
+	/**
+	 * Date the cluster was last changed
+	 * @var string
+	 * @access public
+	 */	
+	public $dateChanged; 
 	
 	public function Cluster($id,$uid=false) {
 		// Get Cluster data
